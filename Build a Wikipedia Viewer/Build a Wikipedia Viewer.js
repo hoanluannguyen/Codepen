bc7412c773a92d4caefce3738f2a92162ff8fcfe
David <hoanluannguyen@gmail.com>
2017-05-30 15:16:24 -0500

    almost

--- Build a Wikipedia Viewer/Build a Wikipedia Viewer.js
+++ Build a Wikipedia Viewer/Build a Wikipedia Viewer.js
@@ -1,23 +1,30 @@
 $(document).ready(function(){
-  $('#searh').click(function(){
-    var searchTerm= $('#searchTerm').val();
-    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+ $('#searchBtn').click(function(){
+   var searchTerm = $('#searchTerm').val();
+ var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchTerm+"&format=json&callback=?";
 
-    $.ajax({
-      type: "GET",
-      url: url,
-      async: false,
-      dataType: "json",
-      success: function(data){
-        console.log(url);
-      },
-      error: function(errorMessage){
-        alert("Error");
-        // try again later!!
-      }
-    });
-  });
+   $.ajax({
+     type: 'GET',
+     url: url,
+     async: false,
+     dataType: 'json',
+     success: function(data){
+       $('#output').html('');
+       for (i=0; i<data[1].length; i++){
+      $('#output').append("<li><a href="+data[3][i]+" target='_blank'>"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
+       }
+     },
+     error: function(errorMessage){
+       alert('Error');
+       // need debug
+     }
+
+          });
+ });
 });
+
+
+
 
 
   /*       $('#btn').click(function() {
@@ -35,4 +42,3 @@ $(document).ready(function(){
          $('#display-result').append('<p>'+apiResult.query.search[i].title+'</p>');
     }
     */
- 
