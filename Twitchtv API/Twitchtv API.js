6eb945010bee9a198bbc3389847e968dab12e010
David <hoanluannguyen@gmail.com>
2017-05-31 16:54:32 -0500

    clear

--- Twitchtv API/Twitchtv API.js
+++ Twitchtv API/Twitchtv API.js
@@ -1,27 +1,4 @@
-$(document).ready(function(){
- $('#searchBtn').click(function(){
-   var searchTerm = $('#searchTerm').val();
- var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchTerm+"&format=json&callback=?";
 
-   $.ajax({
-     type: 'GET',
-     url: url,
-     async: false,
-     dataType: 'json',
-     success: function(data){
-       $('#output').html('');
-       for (i=0; i<data[1].length; i++){
-      $('#output').append("<li><a href="+data[3][i]+" target='_blank'>"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
-       }
-     },
-     error: function(errorMessage){
-       alert('Error');
-       // need debug
-     }
-
-          });
- });
-});
 
 
 
