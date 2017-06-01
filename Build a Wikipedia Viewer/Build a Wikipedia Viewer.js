354a9b25f25cd3c5d44b77a243d8fa9fcf5696d1
David <hoanluannguyen@gmail.com>
2017-06-01 17:35:35 -0500

    debug

--- Build a Wikipedia Viewer/Build a Wikipedia Viewer.js
+++ Build a Wikipedia Viewer/Build a Wikipedia Viewer.js
@@ -1,38 +1,36 @@
-$(document).ready(function(){
- $('#searchBtn').click(function(){
-   var searchTerm = $('#searchTerm').val();
- var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchTerm+"&format=json&callback=?";
+$(document).ready(function() {
+    $('#searchBtn').click(function() {
+        var searchTerm = $('#searchTerm').val();
+        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
 
-   $.ajax({
-     type: 'GET',
-     url: url,
-     async: false,
-     dataType: 'json',
-     success: function(data){
-       $('#output').html('');
-       for (i=0; i<data[1].length; i++){
- $('#output').append("<div class='shadowBox'><li><a href="+data[3][i]+" target='_blank'>"+data[1][i]+"</a><p>"+data[2][i]+"</p></li></div>");
-  }
-},
-// class ='shadowBox' to change background color and justify back ground
-error: function(errorMessage){
-  alert('Error');
-  // need debug
-}
-     error: function(errorMessage){
-       alert('Error');
-       // need debug
-     }
-
-          });
- });
+        $.ajax({
+            type: 'GET',
+            url: url,
+            async: false,
+            dataType: 'json',
+            success: function(data) {
+                $('#output').html('');
+                for (i = 0; i < data[1].length; i++) {
+                    $('#output').append("<div class='shadowBox'><li><a href=" +
+                        data[3][i] + " target='_blank'>" +
+                        data[1][i] + "</a><p>" +
+                        data[2][i] + "</p></li></div>");
+                }
+            },
+            // class ='shadowBox' to change background color and justify back ground
+            error: function(errorMessage) {
+                alert('Let the guy coded me know that I broke');
+                // need debug
+            }
+        });
+    });
 });
 
 
 
 
 
-  /*       $('#btn').click(function() {
+/*       $('#btn').click(function() {
            $.ajax({
                url: 'http://en.wikipedia.org/w/api.php',
                data: { action: 'query', list: 'search', srsearch: $("input[name=Wikipedia]").val(), format: 'json' },
