adc560f9562aa1ffc713e1a41d73bbcb74b7ee5c
David <hoanluannguyen@gmail.com>
2017-06-06 20:31:49 -0500

    source code

--- Twitchtv API/Twitchtv API.js
+++ Twitchtv API/Twitchtv API.js
@@ -1,5 +1,6 @@
 // run jQuery
 $(document).ready(function(){
+  var following=[];
   // stream info ad status API call
   var url= "https://api.twitch.tv/kraken/streams/freecodecamp";
   $.getJSON(url,function(data1){
@@ -9,8 +10,30 @@ $(document).ready(function(){
     else{
       $("#fccStatus").html("Free Code Camp is currently ONLINE")
     }
-
-
-
   });
+  var followerURL ="https://api.twitch.tv/kraken/users/freecodecamp/follows/channels/"
+  $.getJSON(followerURL, function(data2){
+    for (var i=0; i<data2.follows.length; i++ ){
+      var displayName= data2.follows[i].channel.display_name;
+      following.push(displayName);
+    }
+    following.push('name1');
+    following.push('name2');
+    following.push('ESL_SC2');
+
+    for(var i=0; i<following.length; i++){
+      var url2='https://api.twitch.tv/kraken/streams/' + following[i] + '/?callback=?';
+
+      $.getJSON(url2).done(function(data3){
+        var logo;
+        var status;
+        var name;
+        if(data3.error){
+          logo="";
+          name = data2.message;
+          status = data2.error;
+        }
+      });
+    }
+  })
 });
